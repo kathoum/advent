26531d761b8acad66b99592b158d912a5cb15a8b
Saccavino Dario (UniCredit) <dariomarino.saccavino@unicredit.eu>
2020-03-03 18:15:20 +0100

    Day 10 (v.2)

--- day10.rs
+++ day10.rs
@@ -55,11 +55,36 @@ fn main() -> std::io::Result<()> {
     while asteroids.len() > 1 {
         let mut delenda: Vec<_> = asteroids.iter().filter(|ast| visible(&asteroids, &station, &ast)).copied().collect();
         delenda.sort_by(|ast1, ast2| {
+            /*
+            - prima quelli con x>0, poi x<0
+            - se x >= 0, ordino crescente per m=y/x
+              cmp(y1/x1, y2/x2) = cmp(y1*x2, x1*y2)
+            - se x < 0, ordino crescente per m=y/x
+              cmp(y1/x1, y2/x2) = cmp(y1*x2, x1*y2)
+            - a parità di m, ordine crescente per |x|+|y|
+            */
+            let (x1, y1) = (ast1.0 - station.0, ast1.1 - station.1);
+            let (x2, y2) = (ast2.0 - station.0, ast2.1 - station.1);
+            fn quadrant(x: i32, y: i32) -> i32 {
+                use std::cmp::Ordering;
+                match (x.cmp(&0), y.cmp(&0)) {
+                    (Ordering::Equal, Ordering::Less) => 1,
+                    (Ordering::Greater, _) => 2,
+                    (Ordering::Equal, Ordering::Greater) => 3,
+                    (Ordering::Less, _) => 4,
+                    (Ordering::Equal, Ordering::Equal) => panic!("Can't shoot the station!")
+                }
+            }
+            let tup1 = (quadrant(x1, y1), y1 * x2, x1.abs() + y1.abs());
+            let tup2 = (quadrant(x2, y2), y2 * x1, x2.abs() + y2.abs());
+            tup1.cmp(&tup2)
+            /*
             let (x1, y1) = (ast1.0 - station.0, ast1.1 - station.1);
             let key1 = (x1 as f64).atan2(y1 as f64);
             let (x2, y2) = (ast2.0 - station.0, ast2.1 - station.1);
             let key2 = (x2 as f64).atan2(y2 as f64);
             key2.partial_cmp(&key1).unwrap()
+            */
         });
         for (ast, index) in delenda.iter().zip(start..) {
             if index == 200 {
