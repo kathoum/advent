ee280b5d8813cdad151740b78168fdaaf2efdad3
Dario Saccavino <kathoum@gmail.com>
2022-12-18 19:22:52 +0100

    Day 15 (better)

--- src/bin/day15.rs
+++ src/bin/day15.rs
@@ -20,21 +20,29 @@ fn main() {
     let mut ranges = excluded_ranges(&sensors, row);
     for &[_, _, bx, by] in &sensors {
         if by == row {
-            ranges.push_hole(bx);
+            ranges.push_hole(&(bx..=bx));
         }
     }
     println!("In row {} there are {} excluded positions", row, ranges.size());
 
     let size = 4_000_000;
-    for y in 0..=size {
-        let mut ranges = excluded_ranges(&sensors, y);
-        ranges.intersect(0..=size);
-        if ranges.0.len() > 1 {
-            let x = ranges.0[0].end().min(ranges.0[1].end()) + 1;
-            let f = x as i64 * size as i64 + y as i64;
-            println!("The tuning frequency is {f}");
-            break;
+    let mut beacons = Vec::new();
+    for &[sx, sy, bx, by] in &sensors {
+        let r = distance((sx,sy), (bx,by));
+        for d in [sx + sy - r - 1, sx + sy - r - 2, sx + sy + r + 1, sx + sy + r + 2] {
+            let ranges = excluded_diagonal_ranges(&sensors, d);
+            let mut valid_range = RangeUnion(vec![intersection(&(0..=size), &((d-size)..=d))]);
+            valid_range.subtract(&ranges);
+            for x in valid_range.0.into_iter().flatten() {
+                beacons.push((x, d - x));
+            }
         }
+    }
+    beacons.sort();
+    beacons.dedup();
+    for (x,y) in beacons {
+        let freq = x as i64 * size as i64 + y as i64;
+        println!("The tuning frequency is {freq}");
     }
 }
 
@@ -50,6 +58,18 @@ fn excluded_ranges(sensors: &[[i32; 4]], row: i32) -> RangeUnion {
     ranges
 }
 
+fn excluded_diagonal_ranges(sensors: &[[i32; 4]], diagonal: i32) -> RangeUnion {
+    let mut ranges = RangeUnion(Vec::new());
+    for &[sx, sy, bx, by] in sensors {
+        let d = distance((sx,sy), (bx,by));
+        let r = diagonal_range((sx,sy), d, diagonal);
+        if !r.is_empty() {
+            ranges.push(r);
+        }
+    }
+    ranges
+}
+
 fn distance(p: (i32,i32), q: (i32,i32)) -> i32 {
     (p.0-q.0).abs() + (p.1-q.1).abs()
 }
@@ -57,6 +77,18 @@ fn distance(p: (i32,i32), q: (i32,i32)) -> i32 {
 fn range((x,y): (i32,i32), radius: i32, row: i32) -> Range {
     let w = radius - (y-row).abs();
     (x-w)..=(x+w)
+}
+
+// x range of covered cells in the diagonal given by x+y=diagonal
+fn diagonal_range((x,y): (i32,i32), radius: i32, diagonal: i32) -> Range {
+    let h = diagonal - (x + y - radius);
+    if h >= 0 && h <= 2 * radius {
+        let low = x - radius + (h + 1) / 2;
+        let high = x + h / 2;
+        low..=high
+    } else {
+        (x+1)..=x // empty
+    }
 }
 
 fn union(a: &Range, b: &Range) -> Option<Range> {
@@ -92,32 +124,25 @@ impl RangeUnion {
         self.0.push(r);
     }
 
-    pub fn push_hole(&mut self, x: i32) {
-        if let Some((i, r)) = self.0.iter_mut().enumerate().find(|(_,r)| r.contains(&x)) {
-            let a = *r.start()..=(x-1);
-            let b = (x+1)..=*r.end();
-
-            if a.is_empty() && b.is_empty() {
-                self.0.remove(i);
-            } else if a.is_empty() {
-                *r = b;
-            } else if b.is_empty() {
-                *r = a;
-            } else {
-                *r = a;
-                self.0.push(b);
+    pub fn push_hole(&mut self, x: &Range) {
+        let mut result = Vec::new();
+        for r in self.0.drain(..) {
+            let (a,b) = r.into_inner();
+            let r1 = a..=b.min(x.start()-1);
+            if !r1.is_empty() {
+                result.push(r1);
+            }
+            let r2 = a.max(x.end()+1)..=b;
+            if !r2.is_empty() {
+                result.push(r2);
             }
         }
+        self.0 = result;
     }
 
-    pub fn intersect(&mut self, r: Range) {
-        self.0 = self.0.drain(..).filter_map(|x| {
-            let s = intersection(&x, &r);
-            if s.is_empty() {
-                None
-            } else {
-                Some(s)
-            }
-        }).collect();
+    pub fn subtract(&mut self, r: &RangeUnion) {
+        for r in &r.0 {
+            self.push_hole(r);
+        }
     }
 }
